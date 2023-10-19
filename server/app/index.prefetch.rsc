32b1c4da5e38c8749d19f80dedf227f1b13d7c7a
Gusev Mikhail <gsv@feip.co>
2023-10-20 05:45:15 +1000

    Updates

---
+++ server/app/index.prefetch.rsc
@@ -0,0 +1,1 @@
+0:["eEaOTdimvXRG55AOEv4Vt",[[["",{"children":["__PAGE__",{}]},"$undefined","$undefined",true],null,null]]]
---
+++ server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/mikhail/PhpstormProjects/nbcom-test-instagram/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
